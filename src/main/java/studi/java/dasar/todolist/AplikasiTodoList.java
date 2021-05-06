6199a1eb7a4b4bdaeb579c68eca36bb47a628b5b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-06 07:55:44 +0700

    membuat view tiap halaman aplikasi

--- src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
+++ src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
@@ -27,4 +27,26 @@ public class AplikasiTodoList {
     public static void remodeTodoList(){
 
     }
+
+    /**
+     * Menampilkan view todo list
+     */
+    public static void viewShowTodoList(){
+
+    }
+
+    /**
+     * Menampilkan view menambahkan todo list
+     */
+
+    public static void viewAddTodoList(){
+
+    }
+
+    /**
+     * Menampilkan view menghapus todo list
+     */
+    public static void viewRemoveTodoList(){
+
+    }
 }
