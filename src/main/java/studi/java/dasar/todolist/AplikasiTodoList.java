5635bc504c1850f5026b7221c4d097b887fe9ade
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-06 07:50:23 +0700

    buat model data String, buat method business logic

--- src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
+++ src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
@@ -1,7 +1,30 @@
 package studi.java.dasar.todolist;
 
 public class AplikasiTodoList {
+    public static String[] model = new String[10];
+
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList(){
+
+    }
+
+    /**
+     * Menambah todo ke list
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Menghapus todo dari list
+     */
+    public static void remodeTodoList(){
 
     }
 }
