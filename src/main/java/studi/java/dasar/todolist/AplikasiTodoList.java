824ddd1b560a75adba2c8b8b839ebbeacb8fcc78
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-06 08:34:36 +0700

    membuat dan test method menambah todo list dengan resize model

--- src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
+++ src/main/java/studi/java/dasar/todolist/AplikasiTodoList.java
@@ -4,8 +4,8 @@ public class AplikasiTodoList {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-        testShowTodoList();
-
+//        testShowTodoList();
+        testAddTodoList();
     }
 
     /**
@@ -36,9 +36,55 @@ public class AplikasiTodoList {
 
     /**
      * Menambah todo ke list
+     * @param todo sebagai parameter data teks todo list
      */
-    public static void addTodoList(){
+    public static void addTodoList(String todo){
 
+        // buat status untuk modelnya sudah penuh atau belum
+        boolean isFull = true;
+        // Problemnya jika modelnya sudah penuh, artinya harus resize modelnya
+        // cek modelnya udah penuh atau belum
+        for (int i = 0; i < model.length; i++) {
+            if (model[i] == null){
+                // model masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+
+        // cek jika penuh, maka resize ukuran array modelnya yakni 2x lipat
+        // tapi jangan sampai data yang lama hilang
+        if (isFull) {
+            // simpan dulu data lama
+            String[] temporary = model;
+
+            // buat resize model baru 2x lipat
+            model = new String[model.length * 2];
+
+            // masukkan data temp ke model yang baru
+            for (int i = 0; i < temporary.length; i++) {
+                model[i] = temporary[i];
+            }
+        }
+
+        // memasukkan data ke dalam modelnya
+        // masukkan ke index yang data arraynya kosong
+        for (int i = 0; i < model.length; i++) {
+            // cek kalo index modelnya isinya null
+            if (model[i] == null) {
+                model[i] = todo;
+                // break, setelah menambahkan langsung keluar
+                break;
+            }
+        }
+    }
+
+    public static void testAddTodoList(){
+        // test dengan memasukkan data lebih dari 10, karena default modelnya 10
+        for (int i = 0; i < 25; i++) {
+            addTodoList("Contoh todo ke." + i);
+        }
+        showTodoList();
     }
 
     /**
